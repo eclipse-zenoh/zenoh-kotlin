c1012f2b74bc8cfbcde079e53177c6d59f7c520c
Darius Maitia <darius@zettascale.tech>
2023-12-13 17:16:07 +0100

    Fix publication URL

--- zenoh-kotlin/build.gradle.kts
+++ zenoh-kotlin/build.gradle.kts
@@ -123,7 +123,7 @@ kotlin {
         repositories {
             maven {
                 name = "GithubPackages"
-                url = uri("https://maven.pkg.github.com/DariusIMP/zenoh-kotlin")
+                url = uri("https://maven.pkg.github.com/eclipse-zenoh/zenoh-kotlin")
                 credentials {
                     username = System.getenv("GITHUB_ACTOR")
                     password = System.getenv("GITHUB_TOKEN")
