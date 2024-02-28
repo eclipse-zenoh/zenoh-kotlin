92095453768730ea19d2b528780cc701b31838ac
Oussama Teffahi <oussama.teffahi@zettascale.tech>
2024-02-28 18:27:34 +0100

    Add queryable keyexpr to stdout

--- examples/src/main/kotlin/io.zenoh/ZQueryable.kt
+++ examples/src/main/kotlin/io.zenoh/ZQueryable.kt
@@ -61,7 +61,7 @@ class ZQueryable(private val emptyArgs: Boolean) : CliktCommand(
             session.use {
                 key.intoKeyExpr().onSuccess { keyExpr ->
                     keyExpr.use {
-                        println("Declaring Queryable")
+                        println("Declaring Queryable on " + key + "...")
                         session.declareQueryable(keyExpr).res().onSuccess { queryable ->
                             queryable.use {
                                 println("Press CTRL-C to quit...")
