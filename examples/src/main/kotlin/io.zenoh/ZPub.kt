ed182bcd1aa40ebe9ffcdce62d769813d295b68d
Oussama Teffahi <oussama.teffahi@zettascale.tech>
2024-04-18 16:18:50 +0200

    Add index to ZPub payload

--- examples/src/main/kotlin/io.zenoh/ZPub.kt
+++ examples/src/main/kotlin/io.zenoh/ZPub.kt
@@ -67,14 +67,15 @@ class ZPub(private val emptyArgs: Boolean) : CliktCommand(
                                 var idx = 0
                                 while (true) {
                                     Thread.sleep(1000)
-                                    println(
-                                        "Putting Data ('$keyExpr': '[${
+                                    val payload = "[${
                                             idx.toString().padStart(4, ' ')
-                                        }] $value')..."
+                                        }] $value"
+                                    println(
+                                        "Putting Data ('$keyExpr': '$payload')..."
                                     )
                                     attachment?.let {
-                                        pub.put(value).withAttachment(attachment).res()
-                                    } ?: let { pub.put(value).res() }
+                                        pub.put(payload).withAttachment(attachment).res()
+                                    } ?: let { pub.put(payload).res() }
                                     idx++
                                 }
                             }
