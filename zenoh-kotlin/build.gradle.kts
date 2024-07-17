0badc8447b21d5db5671fc44d88913cd1c45da16
Diogo Mendes Matsubara <diogo.matsubara@zettascale.tech>
2024-07-17 15:09:51 +0200

    fix: Update buildDir usage for gradle 8

--- zenoh-kotlin/build.gradle.kts
+++ zenoh-kotlin/build.gradle.kts
@@ -117,7 +117,7 @@ tasks.withType<Test> {
 tasks.whenObjectAdded {
     if ((this.name == "mergeDebugJniLibFolders" || this.name == "mergeReleaseJniLibFolders")) {
         this.dependsOn("cargoBuild")
-        this.inputs.dir(buildDir.resolve("rustJniLibs/android"))
+        this.inputs.dir(layout.buildDirectory.dir("rustJniLibs/android"))
     }
 }
 
