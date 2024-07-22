a8ddb6869d604f6c0bd558401866193c32a2222d
Darius Maitia <darius@zettascale.tech>
2024-07-22 15:09:04 +0200

    feat(api alignment): custom deserializers - wip

--- zenoh-kotlin/src/commonTest/kotlin/io/zenoh/ZBytesTest.kt
+++ zenoh-kotlin/src/commonTest/kotlin/io/zenoh/ZBytesTest.kt
@@ -19,6 +19,7 @@ import io.zenoh.protocol.ZBytes
 import io.zenoh.protocol.into
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
+import kotlin.reflect.typeOf
 import kotlin.test.*
 
 class ZBytesTest {
@@ -77,7 +78,8 @@ class ZBytesTest {
     @Test
     fun serializeAndDeserializeMapViaJNITest() {
         Zenoh.load()
-        val originalMap = mapOf("key1".toByteArray() to "value1".toByteArray(), "key2".toByteArray() to "value2".toByteArray())
+        val originalMap =
+            mapOf("key1".toByteArray() to "value1".toByteArray(), "key2".toByteArray() to "value2".toByteArray())
         val bytes = JNIZBytes.serializeIntoMapViaJNI(originalMap)
         val deserializedMap = JNIZBytes.deserializeIntoMapViaJNI(bytes)
         assertTrue(compareByteArrayMaps(originalMap, deserializedMap))
@@ -127,37 +129,71 @@ class ZBytesTest {
 
     @Test
     fun customDeserializerTest() {
-        val originalMap = mapOf("key1".into() to "value1".into(), "key2".into() to "value2".into())
-        val serializedMap = serializeMap(originalMap)
+        val stringMap = mapOf("key1" to "value1", "key2" to "value2")
+        val zbytesMap = stringMap.map { (k, v) -> k.into() to v.into() }.toMap()
+        val zbytesListOfPairs = stringMap.map { (k, v) -> k.into() to v.into() }
+        val intMap = mapOf(1 to 10, 2 to 20, 3 to 30)
+        val zbytesList = listOf(1.into(), 2.into(), 3.into())
+        val serializedMap = serializeZBytesMap(zbytesMap)
         val bytes: ZBytes = serializedMap.into()
 
-        val deserializedMap = bytes.deserialize<Map<ZBytes, ZBytes>>(mapOf(Map::class.java to ::deserializeMap)).getOrThrow()
-        assertEquals(originalMap, deserializedMap)
+        val customDeserializers = mapOf(
+            typeOf<Map<ZBytes, ZBytes>>() to ::deserializeIntoZBytesMap,
+            typeOf<Map<String, String>>() to ::deserializeIntoStringMap,
+            typeOf<Map<Int, Int>>() to ::deserializeIntoIntMap,
+            typeOf<List<ZBytes>>() to ::deserializeIntoZBytesList,
+            typeOf<List<Pair<ZBytes, ZBytes>>>() to ::deserializeIntoListOfPairs,
+        )
+
+        val deserializedMap = bytes.deserialize<Map<ZBytes, ZBytes>>(customDeserializers).getOrThrow()
+        assertEquals(zbytesMap, deserializedMap)
+
+        val deserializedMap2 = bytes.deserialize<Map<String, String>>(customDeserializers).getOrThrow()
+        assertEquals(stringMap, deserializedMap2)
+
+        val intMapBytes = serializeIntoIntMap(intMap).into()
+        val deserializedMap3 = intMapBytes.deserialize<Map<Int, Int>>(customDeserializers).getOrThrow()
+        assertEquals(intMap, deserializedMap3)
+
+        val serializedZBytesList = serializeZBytesList(zbytesList).into()
+        val deserializedList = serializedZBytesList.deserialize<List<ZBytes>>(customDeserializers).getOrThrow()
+        assertEquals(zbytesList, deserializedList)
+
+        val serializedZBytesPairList = serializeZBytesMap(zbytesListOfPairs.toMap()).into()
+        val deserializedZBytesPairList =
+            serializedZBytesPairList.deserialize<List<Pair<ZBytes, ZBytes>>>(customDeserializers).getOrThrow()
+        assertEquals(zbytesListOfPairs, deserializedZBytesPairList)
     }
 
-    fun serializeMap(testMap: Map<ZBytes, ZBytes>): ByteArray {
+
+    /*--------- Serializers and deserializers for testing purposes. ----------*/
+
+    private fun serializeZBytesMap(testMap: Map<ZBytes, ZBytes>): ByteArray {
         return testMap.map {
             val key = it.key.bytes
             val keyLength = ByteBuffer.allocate(Int.SIZE_BYTES).order(ByteOrder.LITTLE_ENDIAN).putInt(key.size).array()
             val value = it.value.bytes
-            val valueLength = ByteBuffer.allocate(Int.SIZE_BYTES).order(ByteOrder.LITTLE_ENDIAN).putInt(value.size).array()
+            val valueLength =
+                ByteBuffer.allocate(Int.SIZE_BYTES).order(ByteOrder.LITTLE_ENDIAN).putInt(value.size).array()
             keyLength + key + valueLength + value
         }.reduce { acc, bytes -> acc + bytes }
     }
 
-    fun deserializeMap(serializedMap: ByteArray): Map<ZBytes, ZBytes> {
+    private fun deserializeIntoZBytesMap(serializedMap: ByteArray): Map<ZBytes, ZBytes> {
         var idx = 0
         var sliceSize: Int
         val decodedMap = mutableMapOf<ZBytes, ZBytes>()
         while (idx < serializedMap.size) {
-            sliceSize = ByteBuffer.wrap(serializedMap.sliceArray(IntRange(idx, idx + Int.SIZE_BYTES - 1))).order(ByteOrder.LITTLE_ENDIAN).int
+            sliceSize = ByteBuffer.wrap(serializedMap.sliceArray(IntRange(idx, idx + Int.SIZE_BYTES - 1)))
+                .order(ByteOrder.LITTLE_ENDIAN).int
             idx += Int.SIZE_BYTES
 
             val key = serializedMap.sliceArray(IntRange(idx, idx + sliceSize - 1))
             idx += sliceSize
 
             sliceSize = ByteBuffer.wrap(serializedMap.sliceArray(IntRange(idx, idx + Int.SIZE_BYTES - 1))).order(
-                ByteOrder.LITTLE_ENDIAN).int
+                ByteOrder.LITTLE_ENDIAN
+            ).int
             idx += Int.SIZE_BYTES
 
             val value = serializedMap.sliceArray(IntRange(idx, idx + sliceSize - 1))
@@ -166,5 +202,50 @@ class ZBytesTest {
             decodedMap[key.into()] = value.into()
         }
         return decodedMap
+    }
+
+    private fun serializeIntoIntMap(intMap: Map<Int, Int>): ByteArray {
+        val zBytesMap = intMap.map { (k, v) -> k.into() to v.into() }.toMap()
+        return serializeZBytesMap(zBytesMap)
+    }
+
+    private fun deserializeIntoStringMap(serializerMap: ByteArray): Map<String, String> {
+        return deserializeIntoZBytesMap(serializerMap).map { (k, v) -> k.toString() to v.toString() }.toMap()
+    }
+
+    private fun deserializeIntoIntMap(serializerMap: ByteArray): Map<Int, Int> {
+        return deserializeIntoZBytesMap(serializerMap).map { (k, v) ->
+            k.deserialize<Int>().getOrThrow() to v.deserialize<Int>().getOrThrow()
+        }.toMap()
+    }
+
+    private fun serializeZBytesList(list: List<ZBytes>): ByteArray {
+        return list.map {
+            val item = it.bytes
+            val itemLength =
+                ByteBuffer.allocate(Int.SIZE_BYTES).order(ByteOrder.LITTLE_ENDIAN).putInt(item.size).array()
+            itemLength + item
+        }.reduce { acc, bytes -> acc + bytes }
+    }
+
+    private fun deserializeIntoZBytesList(serializedList: ByteArray): List<ZBytes> {
+        var idx = 0
+        var sliceSize: Int
+        val decodedList = mutableListOf<ZBytes>()
+        while (idx < serializedList.size) {
+            sliceSize = ByteBuffer.wrap(serializedList.sliceArray(IntRange(idx, idx + Int.SIZE_BYTES - 1)))
+                .order(ByteOrder.LITTLE_ENDIAN).int
+            idx += Int.SIZE_BYTES
+
+            val item = serializedList.sliceArray(IntRange(idx, idx + sliceSize - 1))
+            idx += sliceSize
+
+            decodedList.add(item.into())
+        }
+        return decodedList
+    }
+
+    private fun deserializeIntoListOfPairs(serializedList: ByteArray): List<Pair<ZBytes, ZBytes>> {
+        return deserializeIntoZBytesMap(serializedList).map { (k, v) -> k to v }
     }
 }
