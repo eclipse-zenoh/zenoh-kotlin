e48c7a62429f26b528fa5c11d7b6aa75288c3b56
Darius Maitia <darius@zettascale.tech>
2024-08-13 11:09:09 +0200

    feat(default args): changing receiver attribute from Queryable, Get and Subscriber to be non optional.

--- zenoh-kotlin/src/commonMain/kotlin/io/zenoh/queryable/Queryable.kt
+++ zenoh-kotlin/src/commonMain/kotlin/io/zenoh/queryable/Queryable.kt
@@ -15,8 +15,6 @@
 package io.zenoh.queryable
 
 import io.zenoh.*
-import io.zenoh.handlers.Callback
-import io.zenoh.handlers.ChannelHandler
 import io.zenoh.handlers.Handler
 import io.zenoh.jni.JNIQueryable
 import io.zenoh.keyexpr.KeyExpr
@@ -68,11 +66,10 @@ import kotlinx.coroutines.channels.Channel
  * @property keyExpr The [KeyExpr] to which the subscriber is associated.
  * @property receiver Optional [R] that is provided when specifying a [Handler] for the subscriber.
  * @property jniQueryable Delegate object in charge of communicating with the underlying native code.
- * @constructor Internal constructor. Instances of Queryable must be created through the [Builder] obtained after
- * calling [Session.declareQueryable] or alternatively through [newBuilder].
+ * @constructor Internal constructor.
  */
 class Queryable<R> internal constructor(
-    val keyExpr: KeyExpr, val receiver: R?, private var jniQueryable: JNIQueryable?
+    val keyExpr: KeyExpr, val receiver: R, private var jniQueryable: JNIQueryable?
 ) : AutoCloseable, SessionDeclaration {
 
     fun isValid(): Boolean {
@@ -86,98 +83,6 @@ class Queryable<R> internal constructor(
 
     override fun close() {
         undeclare()
-    }
-
-    companion object {
-
-        /**
-         * Creates a new [Builder] associated to the specified [session] and [keyExpr].
-         *
-         * @param session The [Session] from which the queryable will be declared.
-         * @param keyExpr The [KeyExpr] associated to the queryable.
-         * @return An empty [Builder] with a default [ChannelHandler] to handle the incoming samples.
-         */
-        fun newBuilder(session: Session, keyExpr: KeyExpr): Builder<Channel<Query>> {
-            return Builder(session, keyExpr, handler = ChannelHandler(Channel()))
-        }
-    }
-
-    /**
-     * Builder to construct a [Queryable].
-     *
-     * Either a [Handler] or a [Callback] must be specified. Note neither of them are stackable and are mutually exclusive,
-     * meaning that it is not possible to specify multiple callbacks and/or handlers, the builder only considers the
-     * last one specified.
-     *
-     * @param R Receiver type of the [Handler] implementation. If no handler is provided to the builder, R will be [Unit].
-     * @property session [Session] to which the [Queryable] will be bound to.
-     * @property keyExpr The [KeyExpr] to which the queryable is associated.
-     * @property callback Optional callback that will be triggered upon receiving a [Query].
-     * @property handler Optional handler to receive the incoming queries.
-     * @constructor Creates a Builder. This constructor is internal and should not be called directly. Instead, this
-     * builder should be obtained through the [Session] after calling [Session.declareQueryable].
-     */
-    class Builder<R> internal constructor(
-        private val session: Session,
-        private val keyExpr: KeyExpr,
-        private var callback: Callback<Query>? = null,
-        private var handler: Handler<Query, R>? = null
-    ) : Resolvable<Queryable<R>> {
-        private var complete: Boolean = false
-        private var onClose: (() -> Unit)? = null
-
-        private constructor(other: Builder<*>, handler: Handler<Query, R>?) : this(other.session, other.keyExpr) {
-            this.handler = handler
-            this.complete = other.complete
-            this.onClose = other.onClose
-        }
-
-        private constructor(other: Builder<*>, callback: Callback<Query>?) : this(other.session, other.keyExpr) {
-            this.callback = callback
-            this.complete = other.complete
-            this.onClose = other.onClose
-        }
-
-        /** Change queryable completeness. */
-        fun complete(complete: Boolean) = apply { this.complete = complete }
-
-        /** Specify an action to be invoked when the [Queryable] is undeclared. */
-        fun onClose(action: () -> Unit): Builder<R> {
-            this.onClose = action
-            return this
-        }
-
-        /** Specify a [Callback]. Overrides any previously specified callback or handler. */
-        fun with(callback: Callback<Query>): Builder<Unit> = Builder(this, callback)
-
-        /** Specify a [Handler]. Overrides any previously specified callback or handler. */
-        fun <R2> with(handler: Handler<Query, R2>): Builder<R2> = Builder(this, handler)
-
-        /** Specify a [Channel]. Overrides any previously specified callback or handler. */
-        fun with(channel: Channel<Query>): Builder<Channel<Query>> = Builder(this, ChannelHandler(channel))
-
-        /**
-         * Resolve the builder, creating a [Queryable] with the provided parameters.
-         *
-         * @return A [Result] with the newly created [Queryable].
-         */
-        override fun wait(): Result<Queryable<R>> = runCatching {
-            require(callback != null || handler != null) { "Either a callback or a handler must be provided." }
-            val resolvedCallback = callback ?: Callback { t: Query -> handler?.handle(t) }
-            val resolvedOnClose = fun() {
-                handler?.onClose()
-                onClose?.invoke()
-            }
-            return session.run {
-                resolveQueryable(
-                    keyExpr,
-                    resolvedCallback,
-                    resolvedOnClose,
-                    handler?.receiver(),
-                    complete
-                )
-            }
-        }
     }
 }
 
