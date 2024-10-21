3e3078d6b35ba9bf3b2b9f3557f599fcb2632b6e
Darius Maitia <darius@zettascale.tech>
2024-10-21 00:40:02 -0300

    ZSerialize and ZDeserialize KDocs

--- zenoh-kotlin/src/commonMain/kotlin/io/zenoh/ext/ZSerialize.kt
+++ zenoh-kotlin/src/commonMain/kotlin/io/zenoh/ext/ZSerialize.kt
@@ -5,7 +5,68 @@ import io.zenoh.jni.JNIZBytes.serializeViaJNI
 import kotlin.reflect.typeOf
 
 /**
- * Serialize an element of type [T] into a [ZBytes].
+ * Serializes an element of type [T] into a [ZBytes].
+ *
+ * This function supports the following types:
+ * - [Boolean]
+ * - [Byte]
+ * - [Short]
+ * - [Int]
+ * - [Long]
+ * - [Float]
+ * - [Double]
+ * - [UByte]
+ * - [UShort]
+ * - [UInt]
+ * - [ULong]
+ * - [List]
+ * - [String]
+ * - [ByteArray]
+ * - [Map]
+ * - [Pair]
+ * - [Triple]
+ *
+ * **NOTE**
+ *
+ * This serialization utility can be used across the Zenoh ecosystem with Zenoh
+ * versions based on other supported languages such as Rust, Python, C and C++.
+ * This works when the types are equivalent (a `Byte` corresponds to an `i8` in Rust, a `Short` to an `i16`, etc).
+ *
+ * ### Examples
+ *
+ * For a Boolean:
+ * ```
+ * val input: Boolean = true
+ * val zbytes = zSerialize(input).getOrThrow()
+ * val output = zDeserialize<Boolean>(zbytes).getOrThrow()
+ * check(input == output)
+ * ```
+ *
+ * For a List:
+ * ```
+ * val input: List<Int> = listOf(1, 2, 3, 4, 5)
+ * val zbytes = zSerialize(input).getOrThrow()
+ * val output = zDeserialize<List<Int>>(zbytes).getOrThrow()
+ * check(input == output)
+ * ```
+ *
+ * For a nested list:
+ * ```
+ * val input: List<List<Int>> = listOf(listOf(1, 2, 3))
+ * val zbytes = zSerialize(input).getOrThrow()
+ * val output = zDeserialize<List<List<Int>>>(zbytes).getOrThrow()
+ * check(input == output)
+ * ```
+ *
+ * For a combined list of maps:
+ * ```
+ * val input: List<Map<String, Int>> = listOf(mapOf("a" to 1, "b" to 2))
+ * val zbytes = zSerialize(input).getOrThrow()
+ * val output = zDeserialize<List<Map<String, Int>>>(zbytes).getOrThrow()
+ * check(input == output)
+ * ```
+ *
+ * For additional examples, checkout the [ZBytes examples](https://github.com/eclipse-zenoh/zenoh-kotlin/blob/main/examples/src/main/kotlin/io.zenoh/ZBytes.kt).
  *
  * @see ZBytes
  * @return a [Result] with the serialized [ZBytes].
